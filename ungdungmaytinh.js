c6ba1209d2389d5c6d6d7eae1261542d90afa389
dungq <dungqa98@gmail.com>
2019-01-11 09:34:23 +0700

    ahihi

---
+++ ungdungmaytinh.js
@@ -0,0 +1,14 @@
+var a=document.getElementById("so1");
+var b=document.getElementById("so2");
+function cong(a,b) {
+    return (a+b);
+}
+function tru(a,b) {
+    return (a-b);
+}
+function nhan(a,b) {
+    return (a*b);
+}
+function chia(a,b) {
+    return (a/b);
+}
