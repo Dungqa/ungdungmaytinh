c1cfea32e86b4e9ba8b798f4e0cc48f50d0ab6a0
dungq <dungqa98@gmail.com>
2019-01-11 11:17:06 +0700

    ahihi

--- ungdungmaytinh.js
+++ ungdungmaytinh.js
@@ -1,14 +1,26 @@
 var a=document.getElementById("so1");
 var b=document.getElementById("so2");
 function cong(a,b) {
-    return (a+b);
+    var ketqua= a+b;
+    var y=document.getElementById('ketqua');
+    y.innerHTML=("Result:"+" "+ketqua);
+    console.log(ketqua);
 }
 function tru(a,b) {
-    return (a-b);
+    var ketqua= a-b;
+    var y=document.getElementById('ketqua');
+    y.innerHTML=("Result:"+" "+ketqua);
+    console.log(ketqua);
 }
 function nhan(a,b) {
-    return (a*b);
+    var ketqua= a*b;
+    var y=document.getElementById('ketqua');
+    y.innerHTML=("Result:"+" "+ketqua);
+    console.log(ketqua);
 }
 function chia(a,b) {
-    return (a/b);
+    var ketqua= a/b;
+    var y=document.getElementById('ketqua');
+    y.innerHTML=("Result:"+" "+ketqua);
+    console.log(ketqua);
 }
